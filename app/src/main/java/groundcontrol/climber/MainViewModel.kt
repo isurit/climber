7ffc1b97117467640fff121b243391859c76c1a9
isurit <isurit@naver.com>
2018-12-21 01:40:10 +0900

    Practice#1 MVVM with LiveData

---
+++ app/src/main/java/groundcontrol/climber/MainViewModel.kt
@@ -0,0 +1,26 @@
+package groundcontrol.climber
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.LiveData
+
+class MainViewModel : ViewModel() {
+    private var users: MutableLiveData<String>? = null
+    private var userData = "BaseData"
+    fun getUsers(): LiveData<String>? {
+        if (users == null) {
+            users = MutableLiveData()
+            loadUsers()
+        }
+        return users
+    }
+    private fun loadUsers(){
+        users?.value = userData
+
+    }
+
+    fun setData(text: String) {
+        userData = text
+        loadUsers()
+    }
+}
